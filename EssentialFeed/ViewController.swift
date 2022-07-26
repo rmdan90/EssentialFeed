012cf1e6d503a5bd0e7c13af86edfd0a14a62ee3
rmdan <rmdan90@gmail.com>
2022-07-26 18:36:45 +0300

    Initial Commit

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Ramadan on 26/07/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
